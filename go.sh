b018d78cdfd03f6e761cd11fd8dbbb82daaa6284
Job Stoit <jobstoit@Jobs-MacBook-Pro.local>
2019-06-16 00:43:56 +0200

    finising touches on go script

--- go.sh
+++ go.sh
@@ -1,6 +1,7 @@
 # install go
 sudo curl https://dl.google.com/go/go1.12.1.darwin-amd64.tar.gz | sudo tar -C /usr/local -xz
 ln -f -s /usr/local/go/bin/go /usr/local/bin/go 
-GOPATH=$HOME/.go
+printf "export GOPATH=\$HOME/.go\nexport GOROOT=/usr/local/go\n" >> $HOME/.bash_profile 
+source $HOME/.bash_profile
 mkdir -p $GOPATH/bin
 sudo curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
