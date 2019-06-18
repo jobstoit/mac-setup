5967033ec1bb5e190e910a10581f914dcaf38be8
Job Stoit <jobstoit@Jobs-MBP.home>
2019-06-19 00:17:36 +0200

    refinement

--- neovim.sh
+++ neovim.sh
@@ -1,6 +1,6 @@
 # Install neovim
 curl -s -L https://github.com/neovim/neovim/releases/download/v0.3.7/nvim-macos.tar.gz | sudo tar -C /usr/local -zx   
-sudo ln -f -s /usr/local/nvim-osx64/bin/nvim /usr/local/bin/nvim
+ln -f -s /usr/local/nvim-osx64/bin/nvim /usr/local/bin/nvim
 
 git clone https://github.com/jobstoit/nvim.git $HOME/.vim
 mkdir -p $HOME/.config/nvim && echo -e "set runtimepath^=\$HOME/.vim runtimepath+=\$HOME/.vim/after\nlet &packpath = &runtimepath\nsource \$HOME/.vim/vimrc" > $HOME/.config/nvim/init.vim
