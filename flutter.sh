257376f2a858c171ddf903196267af8716ef0409
Job Stoit <jobstoit@Jobs-MBP.home>
2019-06-19 00:42:38 +0200

    refine more

--- flutter.sh
+++ flutter.sh
@@ -1,6 +1,8 @@
 # install flutter
+sudo echo 'Installing flutter...'
 curl -o temp.zip -L https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v1.5.4-hotfix.2-stable.zip
 unzip -q -d $HOME temp.zip
 rm temp.zip
-mv $HOME/flutter $HOME/.mob
-ln -f -s $HOME/.mob/bin/flutter /usr/local/bin/flutter
+sudo mv $HOME/flutter /usr/local/flutter
+ln -f -s /usr/local/flutter/bin/flutter /usr/local/bin/flutter
+flutter
--- go.sh
+++ go.sh
@@ -1,5 +1,6 @@
 # install go
-sudo curl https://dl.google.com/go/go1.12.1.darwin-amd64.tar.gz | sudo tar -C /usr/local -xz
+sudo echo 'Installing go...'
+curl https://dl.google.com/go/go1.12.1.darwin-amd64.tar.gz | sudo tar -C /usr/local -xz
 ln -f -s /usr/local/go/bin/go /usr/local/bin/go 
 printf "export GOPATH=\$HOME/.go\nexport GOROOT=/usr/local/go\n" >> $HOME/.bash_profile 
 source $HOME/.bash_profile
