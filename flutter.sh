2d0acd2be7e7841f7222d02b8fdf37c03df4f8e0
Job Stoit <jobstoit@Jobs-MBP.home>
2019-06-19 14:07:10 +0200

    add golangci-lint

--- flutter.sh
+++ flutter.sh
@@ -5,4 +5,14 @@ unzip -q -d $HOME temp.zip
 rm temp.zip
 sudo mv $HOME/flutter /usr/local/flutter
 ln -f -s /usr/local/flutter/bin/flutter /usr/local/bin/flutter
+
+brew update
+brew install --HEAD usbmuxd
+brew link usbmuxd
+brew install --HEAD libimobiledevice
+brew install ideviceinstaller
+brew install ios-deploy
+brew install cocoapods
+pod setup
+
 flutter
--- go.sh
+++ go.sh
@@ -1,8 +1,10 @@
 # install go
 sudo echo 'Installing go...'
 curl https://dl.google.com/go/go1.12.1.darwin-amd64.tar.gz | sudo tar -C /usr/local -xz
-ln -f -s /usr/local/go/bin/go /usr/local/bin/go 
+ln -f -s /usr/local/go/bin/* /usr/local/bin/
+curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh| sh -s -- -b /usr/local/go/bin v1.17.1
+curl https://raw.githubusercontent.com/golang/dep/master/install.sh | INSTALL_DIRECTORY=/usr/local/go/bin sh
+ln -f -s /usr/local/go/bin/* /usr/local/bin/
 printf "export GOPATH=\$HOME/.go\nexport GOROOT=/usr/local/go\n" >> $HOME/.bash_profile 
 source $HOME/.bash_profile
 mkdir -p $GOPATH/bin
-sudo curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
