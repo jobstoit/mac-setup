ace0cc3e06609e4afa91fcfc59f559f6830ce457
Job Stoit <jobstoit@Jobs-MBP.home>
2019-06-29 17:56:25 +0200

    add GRPC script

--- flutter.sh
+++ flutter.sh
@@ -1,10 +1,9 @@
 # install flutter
 sudo echo 'Installing flutter...'
-curl -o temp.zip -L https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v1.5.4-hotfix.2-stable.zip
-unzip -q -d $HOME temp.zip
-rm temp.zip
-sudo mv $HOME/flutter /usr/local/flutter
-ln -f -s /usr/local/flutter/bin/flutter /usr/local/bin/flutter
+curl -o temp.zip -L https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v1.5.4-hotfix.2-stable.zip && \
+	sudo unzip -q -d /usr/local/ temp.zip && \
+	rm temp.zip && \
+	ln -f -s /usr/local/flutter/bin/* /usr/local/bin/
 
 brew update
 brew install --HEAD usbmuxd
---
+++ grpc.sh
@@ -0,0 +1,8 @@
+# Install gRPC
+sudo echo 'installing gRPC...'
+curl -o temp.zip -L https://github.com/protocolbuffers/protobuf/releases/download/v3.8.0/protoc-3.8.0-osx-x86_64.zip && \
+	sudo unzip -q -d /usr/local/protoc temp.zip && \
+	rm temp.zip &&
+	ln -f -s /usr/local/protoc/bin/* /usr/local/bin/
+
+protoc --version
